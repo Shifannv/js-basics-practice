8580fbf2903e2feeec65bb8e85822b10a2ac41f0
Shifan Nv <m.shifan.nv@gmail.com>
2026-06-11 18:30:28 +0530

    updated palidrome

--- Practical-dones/palidrome-checking-names.js
+++ Practical-dones/palidrome-checking-names.js
@@ -1,11 +1,14 @@
-const name = "malayalam"
+const name = "shifan"
 let longs = ''
 for( let i = name.length -1 ; i >= 0 ; i--){
       longs =  longs + name[i]
-        
-      if ( name == longs || name !== longs){     
+         result = false
+      if ( result = (name == longs)){
+        result = true
+    } 
+    else if(!result) {    
     }
 }
-
-console.log("its palidrome"|| "it's not a palidrome "); 
+console.log( !result, " : it's not a palidrome");
+console.log( result , " :  its palidrome " ); 
 console.log(longs);
