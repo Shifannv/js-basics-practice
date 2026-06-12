3419fa1bc1fe01b4c743936fc551d4f49ccbc34a
Shifan Nv <m.shifan.nv@gmail.com>
2026-06-12 14:35:33 +0530

    finding the greatest

---
+++ Practical-dones/finding-gthan-arry-to-nearby-ary.js
@@ -0,0 +1,17 @@
+const unOrderedList = [30,3,35,50,100,60,80,85]
+const list = (unOrderedList.sort((a,b) => a-b))
+const  biggest = list[1] 
+console.log(list[1]);
+
+// for( let i = 0 ; i<list.length ; i++){
+//      // let lists = list[i]
+//      // console.log(lists);
+//      if( list[i] > biggest ){
+//          biggest = list[i]
+//      }
+     
+// }
+
+// console.log(biggest);
+
+
