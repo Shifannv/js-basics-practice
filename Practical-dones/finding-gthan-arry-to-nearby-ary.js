bb2ef64a0d10f659094c933a5fe5b786397fecbc
Shifan Nv <m.shifan.nv@gmail.com>
2026-06-13 14:38:16 +0530

    updated

--- Practical-dones/finding-gthan-arry-to-nearby-ary.js
+++ Practical-dones/finding-gthan-arry-to-nearby-ary.js
@@ -3,15 +3,3 @@ const list = (unOrderedList.sort((a,b) => a-b))
 const  biggest = list[1] 
 console.log(list[1]);
 
-// for( let i = 0 ; i<list.length ; i++){
-//      // let lists = list[i]
-//      // console.log(lists);
-//      if( list[i] > biggest ){
-//          biggest = list[i]
-//      }
-     
-// }
-
-// console.log(biggest);
-
-
